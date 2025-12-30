4efda881192fdac75d338b852272f89fe67bde67
GiovanniSeloto <161782731+GiovanniSeloto@users.noreply.github.com>
2025-12-30 10:34:19 -0300

    feat: complementa implementação nas classes Iphone, Nokia, Smartphone e Program

--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+Iphone iphone = new Iphone("1191824501", "Iphone 15", "73829", "128");
+iphone.Ligar();
+iphone.InstalarAplicativo("WhatsApp");
+
+Console.WriteLine("\n");
+
+Nokia nokia = new Nokia("11974832618", "S25 Ultra", "74819", "256");
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("Telegram");
